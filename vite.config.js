340be6a826a2249c2b7cfd47a8666f4f6e45deec
chuting0407 <tiffany970407@gmail.com>
2026-04-27 20:26:03 +0800

    修改6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/期中202604/', 
+  base: '/20260501/', // ⚠️ 這一行最重要！必須是你的 GitHub 倉庫名稱，前後都要有斜線
 })
